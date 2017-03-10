4217f9c3860c99c1523c8447f8c1017a3b47748f
5742G\Acer <2016vda@gmail.com>
2017-03-10 09:17:45 +0200

    four pages1

---
+++ js/main.js
@@ -0,0 +1,161 @@
+jQuery(document).ready(function () {
+
+    $('.rocket').on('click touchend', function () {
+        $('html, body').animate({'scrollTop': 0}, 800, 'swing');
+        return false;
+    });
+
+    $('.like').on('click', function () {
+        $(this).toggleClass('active');
+        return false;
+    });
+
+    $('.cabinet-ul a').on('click', function () {
+        $('.cabinet-block > div').hide();
+        $('.cabinet-block > [class="'+$(this).attr('data-tab')+'"]').show();
+        return false;
+    });
+
+        $('.slick_w').slick({
+            arrows: true,
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,  
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    
+    $('.carousel').slick({
+        dots: false,
+        infinite: false,
+        speed: 500,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.carousel-partners').slick({
+        dots: false,
+        infinite: false,
+        speed: 400,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.vertical-carousel').rcarousel({
+        orientation: 'vertical',
+        visible: 4,
+        step: 2,
+        width: 225,
+        height: 144,
+        navigation: {
+            prev: ".prev",
+            next: ".next"
+        }
+    });
+
+    $('.groups-carousel').rcarousel({
+        orientation: 'vertical',
+        visible: 4,
+        step: 2,
+        width: 225,
+        height: 144,
+        navigation: {
+            prev: ".prev2",
+            next: ".next2"
+        }
+    });
+
+    $(".slider").slider({
+        range: true,
+        min: 18,
+        max: 100,
+        values: [ 18, 80 ],
+        slide: function( event, ui ) {
+            var values = $(".slider").slider( "option", "values" );
+            $( "#slider-result-1" ).html(values[0] + ' лет');
+            $( "#slider-result-2" ).html(values[1] + ' лет');
+        },
+        change: function(event, ui) {
+            var values = $(".slider").slider( "option", "values" );
+            $( "#slider-result-1" ).html(values[0] + ' лет');
+            $( "#slider-result-2" ).html(values[1] + ' лет');
+        }
+    });
+});
