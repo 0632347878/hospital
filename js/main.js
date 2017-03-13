0d73cad01a0113a53cda2c270cd22521289d2f72
5742G\Acer <2016vda@gmail.com>
2017-03-13 18:01:01 +0200

    Images five teamplate

--- js/main.js
+++ js/main.js
@@ -158,4 +158,69 @@ jQuery(document).ready(function () {
             $( "#slider-result-2" ).html(values[1] + ' лет');
         }
     });
+
+      $('.vertical-carousel2').rcarousel({
+        orientation: 'vertical',
+        visible: 4,
+        step: 1,
+        width: 225,
+        height: 144,
+        loop: true,
+        // auto: {enabled: true},
+        navigation: {
+          prev: ".prev2",
+          next: ".next2"
+        },
+      });
+      $('.vertical-carousel3').rcarousel({
+        orientation: 'vertical',
+        visible: 4,
+        step: 1,
+        width: 225,
+        height: 144,
+        loop: true,
+        // auto: {enabled: true},
+        navigation: {
+          prev: ".prev3",
+          next: ".next3"
+        },
+      });
+  jQuery(function($) {
+    $('.vertical-carousel4').rcarousel({
+      orientation: 'vertical',
+      visible: 5,
+      step: 1,
+      width: 260,
+      height: 120,
+      navigation: {
+        prev: ".prev4",
+        next: ".next4"
+      },
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: false
+              }
+          },
+          {
+              breakpoint: 600,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+      ]
+    });
+  });
 });
